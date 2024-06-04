e5865f1d4f75f81d332db069c6cfa5fcf44e4e2b
Luís Miguel <luismsantos.contato@gmail.com>
2024-06-03 22:49:29 -0300

    feat: implementa o backend do sistema de login e cadastro

--- src/app/components/primary-input/primary-input.component.ts
+++ src/app/components/primary-input/primary-input.component.ts
@@ -1,51 +1,52 @@
 import { Component, Input, forwardRef, input } from '@angular/core';
-import { ControlValueAccessor, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
+import {
+  ControlValueAccessor,
+  NG_VALUE_ACCESSOR,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
-type InputTypes = "text" | "email" | "senha"| "confirmaSenha" | "telefone"
+type InputTypes = 'text' | 'email' | 'password' | 'password';
 
 @Component({
   selector: 'app-primary-input',
   standalone: true,
-  imports: [
-    ReactiveFormsModule
-  ],
+  imports: [ReactiveFormsModule],
   providers: [
     {
       provide: NG_VALUE_ACCESSOR,
       useExisting: forwardRef(() => PrimaryInputComponent),
-      multi: true
-    }
+      multi: true,
+    },
   ],
   templateUrl: './primary-input.component.html',
-  styleUrl: './primary-input.component.css'
+  styleUrl: './primary-input.component.css',
 })
-export class PrimaryInputComponent implements ControlValueAccessor{
+export class PrimaryInputComponent implements ControlValueAccessor {
+  @Input() type: InputTypes = 'text';
+  @Input() placeholder: string = '';
+  @Input() label: string = '';
+  @Input() inputName: string = '';
 
-    @Input() type: InputTypes = "text";
-    @Input() placeholder: string = "";
-    @Input() label: string = "";
-    @Input() inputName: string = "";
+  value: string = '';
+  onchange: any = () => {};
+  onTouched: any = () => {};
 
-    value: string = ''
-    onchange: any = () => {}
-    onTouched: any = () => {}
+  onInput(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.onchange(value);
+  }
 
-    onInput(event: Event){
-      const value = (event.target as HTMLInputElement).value
-      this.onchange(value)
-    }
+  writeValue(value: any): void {
+    this.value = value;
+  }
 
-    writeValue(value: any): void {
-      this.value = value
-    }
+  registerOnChange(fn: any): void {
+    this.onchange = fn;
+  }
 
-    registerOnChange(fn: any): void {
-      this.onchange = fn
-    }
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
 
-    registerOnTouched(fn: any): void {
-      this.onTouched = fn
-    }
-
-    setDisabledState(isDisabled: boolean): void {}
+  setDisabledState(isDisabled: boolean): void {}
 }
