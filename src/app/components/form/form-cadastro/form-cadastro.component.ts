38471abe2dc81bd80109ab8d306397d421171fed
Luís Miguel <142459590+luismour@users.noreply.github.com>
2024-06-10 08:39:41 -0300

    Update form-cadastro.component.ts

--- src/app/components/form/form-cadastro/form-cadastro.component.ts
+++ src/app/components/form/form-cadastro/form-cadastro.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-form-cadastro',
   standalone: true,
   imports: [
-    MatSlideToggleModule],
+    MatSlideToggleModule, RouterLink],
   templateUrl: './form-cadastro.component.html',
   styleUrl: './form-cadastro.component.css'
 })
