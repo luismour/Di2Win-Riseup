9b6dfbe1dc4ea2525bba9de757dc1f65edb23472
Luís Miguel <142459590+luismour@users.noreply.github.com>
2024-06-10 08:40:02 -0300

    Update form-senha.component.ts

--- src/app/components/form/form-senha/form-senha.component.ts
+++ src/app/components/form/form-senha/form-senha.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-form-senha',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './form-senha.component.html',
   styleUrl: './form-senha.component.css'
 })
